81f770de08491d7fdcc2f458f7456214bade45d6
Curtis Christie <103606492+curtis-christie@users.noreply.github.com>
2025-04-30 22:09:36 -0400

    03 pass

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+	let arr = string.split("");
+	reversed = arr.reverse();
+	let str2 = reversed.join("");
+	return str2;
+	``;
 };
 
 // Do not edit below this line
