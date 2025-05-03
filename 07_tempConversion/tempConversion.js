7910e011857ae9e7c9d25b04cbc349939d5e0b87
Curtis Christie <103606492+curtis-christie@users.noreply.github.com>
2025-05-02 20:18:52 -0400

    07 pass

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degree) {
+	const celsius = (degree - 32) * (5 / 9);
+	return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degree) {
+	const fahrenheit = degree * (9 / 5) + 32;
+	return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
