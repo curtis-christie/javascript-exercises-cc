23e2c39a18bef89ea339e1231248e8a0c77a1ed9
Curtis Christie <103606492+curtis-christie@users.noreply.github.com>
2025-05-09 19:50:07 -0400

    09 pass

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+	const cleanedWord = str
+		.toLowerCase()
+		.split("")
+		.filter((char) => alphanumerical.includes(char))
+		.join("");
+
+	const reversedString = cleanedWord.split("").reverse().join("");
+
+	return cleanedWord === reversedString;
 };
 
 // Do not edit below this line
