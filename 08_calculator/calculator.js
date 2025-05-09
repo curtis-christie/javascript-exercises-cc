01744bc27b00874c8aacca290097c5ff27930a2b
Curtis Christie <103606492+curtis-christie@users.noreply.github.com>
2025-05-09 19:10:09 -0400

    08 pass

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,49 @@
-const add = function() {
-	
+const add = function (a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+	let sum = 0;
+	arr.forEach((num) => {
+		sum += num;
+	});
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+	let product = 1;
+	arr.forEach((num) => {
+		product *= num;
+	});
+	return product;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+	let total = 1;
+	if (num === 0) {
+		return total;
+	} else {
+		for (let a = num; a > 0; a--) {
+			total *= a;
+		}
+	}
+	return total;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
